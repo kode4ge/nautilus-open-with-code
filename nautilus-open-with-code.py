2a00c18ab013f76c840fe6ce3b676a0a8bd81fe8
KodeForge <contact@kode4ge.com>
2025-04-24 01:38:05 -0300

    Add LICENSE

--- nautilus-open-with-code.py
+++ nautilus-open-with-code.py
@@ -1,3 +1,8 @@
+# Copyright (c) 2025 KodeForge IT Solutions
+# 
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
 import logging as log
 import os
 import subprocess
--- test.sh
+++ test.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+# Copyright (c) 2025 KodeForge IT Solutions
+# 
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
 
 #set -x
 
